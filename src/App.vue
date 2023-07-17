a8c88508bf9c7d9efd0efb81a5206ab8d4af9bc1
Courage OmniBarry <135624085+AndreOmniBarry@users.noreply.github.com>
2023-07-17 01:26:19 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@
           </div>
         </div>
         <div class="actions">
-          <button @click="toggleCompleted(task)"> {{ task.completed ? 'Incomplete' : 'Complete' }}</button>
+          <button @click="toggleCompleted(task)"> {{ task.completed ? 'Incomplete' : 'Completed' }}</button>
           <button @click="removeTask(index)">Remove</button>
         </div>
       </li>
